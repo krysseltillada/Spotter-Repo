85c5f2bf2dcef37b9e6642ac1e40a4c4645834d2
im_senpai <emmanmisodiel@gmail.com>
2017-07-11 19:28:10 +0800

    save

---
+++ Spotter/app/src/main/java/com/lmos/spotter/NavigationActivity.java
@@ -0,0 +1,13 @@
+package com.lmos.spotter;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by emman on 7/11/2017.
+ */
+
+public class NavigationActivity extends AppCompatActivity {
+
+
+
+}
